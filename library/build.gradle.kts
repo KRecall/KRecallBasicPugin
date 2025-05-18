f8c4bb54e9e057477829defd9c06f66badbdcdcd
octest <octest8848@qq.com>
2025-05-18 12:40:12 +0800

    KRecall2 Init3

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "io.github.octestx.krecall.plugins.basiclib"
-version = "2.0.0"
+version = "2.0.0F1"
 
 kotlin {
     jvm()
--- library/src/commonMain/kotlin/PluginMetadata.kt
+++ library/src/commonMain/kotlin/PluginMetadata.kt
@@ -1,6 +1,5 @@
 package io.github.octestx.krecall.plugins.basic
 
-import io.github.octestx.basic.multiplatform.common.utils.OS
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -8,12 +7,6 @@ import kotlinx.serialization.Serializable
 data class PluginMetadata(
     @SerialName("plugin_id")
     val pluginId: String,
-
-    @SerialName("support_platform")
-    val supportPlatform: Set<OS.OperatingSystem>,
-
-    @SerialName("support_ui")
-    val supportUI: Boolean,
 
     @SerialName("main_class")
     val mainClass: String
